7aa627b2299c899095cc9db2fff54fe2e2756d69
anirudh-bukka <anirudh.bukka@gmail.com>
2023-01-28 20:45:19 +0530

    Some commits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <BrowserRouter>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+// "react": "^18.2.0",
+//     "react-dom": "^18.2.0",
+//     "react-router-dom": "^6.8.0",
