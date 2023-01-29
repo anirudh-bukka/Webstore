84e403334a8bbda3d1b6c0abeaba67f6af270087
anirudh-bukka <anirudh.bukka@gmail.com>
2023-01-29 17:58:56 +0530

    Created "tile" to render in Home page.

---
+++ frontend/src/components/BrandTile/BrandTile.js
@@ -0,0 +1,12 @@
+import './BrandTile.css'
+
+const BrandTile = () => {
+    return (
+        <div className='BrandTile'>
+            <h3>Product Name</h3>
+            <p>Product Description</p>
+        </div>
+    )
+}
+
+export default BrandTile;
